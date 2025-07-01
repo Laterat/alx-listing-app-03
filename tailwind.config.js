c88e90fef53a185e85b274a6f785f518b5ab0cf5
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-01 14:22:18 +0300

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     screens: {
       sm: '640px',
       md: '768px',
-      lg: '1024px',
+      lg: '1024px',  // tablet breakpoint
       desktop: '1728px', // ✅ custom breakpoint
     },
   },
